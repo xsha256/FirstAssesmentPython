8bf32bccac40fb3b8e97deee4ffdde033790c00b
xsha256 <43494720+xsha256@users.noreply.github.com>
2023-05-12 11:03:01 +0200

    Trabajo

--- contenidocodigo/app.py
+++ contenidocodigo/app.py
@@ -1,4 +1,10 @@
 from flask import Flask, request, render_template
+import os
+import random
+import platform
+from clases.accion import Accion 
+from clases.comedia import Comedia 
+from clases.drama import Drama
 
 app = Flask(__name__,template_folder='html')
 
@@ -9,11 +15,24 @@ def películas():
 @app.route("/películas", methods=['POST'])
 def mostrar_películas():
  # Obtener la película seleccionada por el usuario
+    nombre = request.form['película']
 
  # Insertar el código aquí
-        
+    if nombre == "Acción":
+        anyo = request.form['anyo']
+        protagonista = request.form['protagonista']
+        pelicula_ingresada = Accion(nombre, anyo, protagonista)
+    elif nombre == "Comédia":
+        anyo = request.form['anyo']
+        director = request.form['director']
+        pelicula_ingresada = Comedia(nombre, anyo, director)
+    elif nombre == "Drama":
+        anyo = request.form['anyo']
+        tema = request.form['tema']
+        pelicula_ingresada = Drama(nombre, anyo, tema)
+
  # Renderizar la página de películas con la película seleccionada
- return render_template("películas.html", película=película_ingresada)
+    return render_template("películas.html", película=pelicula_ingresada)
 
 
 if __name__ == '__main__':
